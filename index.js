fd03541f97ec414eb342a7e8094796d8b05b0979
Mohsin Niazi <mohsinkniazi22@gmail.com>
2023-12-02 22:56:35 +0500

    session storage remove from index.js and mysql integration started to store users in database

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const session = require('express-session')
-const customer_routes = require('./router/auth_users.js').authenticated;
+const customer_routes = require('./router/customer.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 // Customer authenication mechanism
 app.use("/customer/auth/*", function auth(req,res,next){
--- router/general.js
+++ router/general.js
@@ -1,7 +1,7 @@
 const express = require('express');
 let books = require("./booksdb.js");
-let isValid = require("./auth_users.js").isValid;
-let users = require("./auth_users.js").users;
+let isValid = require("./customer.js").isValid;
+let users = require("./customer.js").users;
 const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {

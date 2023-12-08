c079aa6e3c00bfba8325e0bdb07e5be65f49b294
Mohsin Niazi <mohsinkniazi22@gmail.com>
2023-12-08 23:09:00 +0500

    morgan logger middleware configured

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import morgan from 'morgan';
 import 'dotenv/config';
 import { authenticated as customer_routes } from './router/customer.js';
 import { general as genl_routes } from './router/general.js';
 import { authenticateUser } from './middleware/auth-user.js';
 
 const app = express();
+
 app.use(express.json());
+app.use(morgan('dev'));
 
 app.get('/test', authenticateUser, (req, res) => {
   return res.status(200).json({ message: 'Authenticated successfully!' });
